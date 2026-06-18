0c33c0429e68eb7b1c0384ed1178e803b76a32dd
Anand <anandkm539@gmail.com>
2026-06-18 12:00:41 +0530

    Fix uploads folder creation

--- routes/visitor.js
+++ routes/visitor.js
@@ -9,20 +9,26 @@ const {
     sendVisitorPassEmail
 } = require('../services/emailService');
 
+const path = require('path');
+
 const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
 
-        cb(null, './uploads/photos');
+        const uploadPath = path.join(__dirname, '../uploads/photos');
+
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath);
     },
 
     filename: function (req, file, cb) {
 
         cb(
             null,
-            Date.now() +
-            '-' +
-            file.originalname
+            Date.now() + '-' + file.originalname
         );
     }
 });
